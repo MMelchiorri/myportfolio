220a1da121e66bfb8f79ef4feff0be61314d7d19
MMelchiorri <mm.melchiorri@gmail.com>
2025-08-04 11:48:32 +0200

    refactor project layout and navigation styles for improved responsiveness and organization

--- src/section/project-section/project.tsx
+++ src/section/project-section/project.tsx
@@ -6,7 +6,22 @@ import styles from "./project.module.css";
 const Project: React.ElementType = () => {
   return (
     <div className={styles.container} id={"myProject"}>
-      sono in projection section
+      <div className={styles.projectContainer}>
+        <div>
+          <h2 className={styles.projectTitle}>Facebook Clone</h2>
+          <p className={styles.projectDescription}>
+            This is a brief description of my project. It showcases the skills
+            and technologies I have learned.
+          </p>
+        </div>
+        <div>
+          <h2 className={styles.projectTitle}>Todo App</h2>
+          <p className={styles.projectDescription}>
+            This is a brief description of my project. It showcases the skills
+            and technologies I have learned.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
