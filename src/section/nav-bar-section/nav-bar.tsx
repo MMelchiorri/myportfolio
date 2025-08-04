05b8e428f9e408f88edb7b2e5b58dd4a197602ad
MMelchiorri <mm.melchiorri@gmail.com>
2025-08-04 12:12:45 +0200

    refactor navigation bar links to simplify structure and enhance styling

--- src/section/nav-bar-section/nav-bar.tsx
+++ src/section/nav-bar-section/nav-bar.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import styles from "./nav-bar.module.css";
 import Link from "next/link";
-import { Typography } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import WorkIcon from "@mui/icons-material/Work";
 import ContactMailIcon from "@mui/icons-material/ContactMail";
@@ -15,38 +14,16 @@ const NavBar: React.FC = () => {
       role="navigation"
       aria-label="Main Navigation"
     >
-      <div className={styles.logo}>
-        <Link href="/" className={styles.link}>
-          Logo
-        </Link>
-      </div>
+      <div className={styles.logo}></div>
       <div className={styles.information}>
         <Link href="#heroSection" className={styles.link}>
-          <Typography variant="h6" className={styles.linkText}>
-            <HomeIcon
-              fontSize="small"
-              style={{ verticalAlign: "middle", marginRight: "0.4rem" }}
-            />
-            About
-          </Typography>
+          <HomeIcon style={{ marginRight: "1rem" }} />
         </Link>
         <Link href="#myProject" className={styles.link}>
-          <Typography variant="h6" className={styles.linkText}>
-            <WorkIcon
-              fontSize="small"
-              style={{ verticalAlign: "middle", marginRight: "0.4rem" }}
-            />
-            My Project
-          </Typography>
+          <WorkIcon style={{ marginRight: "1rem" }} />
         </Link>
         <Link href="#contact" className={styles.link}>
-          <Typography variant="h6" className={styles.linkText}>
-            <ContactMailIcon
-              fontSize="small"
-              style={{ verticalAlign: "middle", marginRight: "0.4rem" }}
-            />
-            Contact
-          </Typography>
+          <ContactMailIcon style={{ marginRight: "1rem" }} />
         </Link>
       </div>
     </div>
