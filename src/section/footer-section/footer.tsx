1f0cc2f38aa74f23c7081d8009cb7bd138f9dcc2
MMelchiorri <mm.melchiorri@gmail.com>
2025-08-04 16:52:23 +0200

    enhance footer with social media links and improve layout styling

--- src/section/footer-section/footer.tsx
+++ src/section/footer-section/footer.tsx
@@ -2,11 +2,27 @@
 
 import React from "react";
 import styles from "./footer.module.css";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import Link from "next/link";
 
 const Footer: React.ElementType = () => {
   return (
     <div className={styles.container} id={"contact"}>
-      sono in footer section
+      <Link
+        className={styles.link}
+        href="https://www.linkedin.com/in/marco-melchiorri/"
+        target="_blank"
+      >
+        <LinkedInIcon />
+      </Link>
+      <Link
+        className={styles.link}
+        href={"https://github.com/MMelchiorri"}
+        target={"_blank"}
+      >
+        <GitHubIcon />
+      </Link>
     </div>
   );
 };
