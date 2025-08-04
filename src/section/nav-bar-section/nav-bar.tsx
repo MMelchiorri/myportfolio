0542908182e55ec20ec6a147b4ba24397b072966
MMelchiorri <mm.melchiorri@gmail.com>
2025-08-04 12:01:25 +0200

    enhance navigation bar with Material UI icons and improve link styling

--- src/section/nav-bar-section/nav-bar.tsx
+++ src/section/nav-bar-section/nav-bar.tsx
@@ -3,6 +3,10 @@
 import React from "react";
 import styles from "./nav-bar.module.css";
 import Link from "next/link";
+import { Typography } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import WorkIcon from "@mui/icons-material/Work";
+import ContactMailIcon from "@mui/icons-material/ContactMail";
 
 const NavBar: React.FC = () => {
   return (
@@ -18,13 +22,31 @@ const NavBar: React.FC = () => {
       </div>
       <div className={styles.information}>
         <Link href="#heroSection" className={styles.link}>
-          About
+          <Typography variant="h6" className={styles.linkText}>
+            <HomeIcon
+              fontSize="small"
+              style={{ verticalAlign: "middle", marginRight: "0.4rem" }}
+            />
+            About
+          </Typography>
         </Link>
         <Link href="#myProject" className={styles.link}>
-          My Project
+          <Typography variant="h6" className={styles.linkText}>
+            <WorkIcon
+              fontSize="small"
+              style={{ verticalAlign: "middle", marginRight: "0.4rem" }}
+            />
+            My Project
+          </Typography>
         </Link>
         <Link href="#contact" className={styles.link}>
-          Contact
+          <Typography variant="h6" className={styles.linkText}>
+            <ContactMailIcon
+              fontSize="small"
+              style={{ verticalAlign: "middle", marginRight: "0.4rem" }}
+            />
+            Contact
+          </Typography>
         </Link>
       </div>
     </div>
