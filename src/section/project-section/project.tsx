50a0f0930be21ab38be543d9c1295b7999740fff
MMelchiorri <mm.melchiorri@gmail.com>
2025-08-05 09:06:00 +0200

    enhance project layout with improved styling and responsive design

--- src/section/project-section/project.tsx
+++ src/section/project-section/project.tsx
@@ -9,31 +9,25 @@ import { Grid } from "@mui/material";
 const Project: React.ElementType = () => {
   return (
     <div className={styles.container} id={"myProject"}>
-      <Grid
-        container
-        className={styles.projectContainer}
-        justifyContent={"center"}
-        gap={40}
-      >
-        <Grid size={{ md: 4 }}>
+      <Grid container className={styles.projectContainer} gap={40}>
+        <Grid size={{ md: 6 }}>
           <Typography variant={"h4"} style={{ whiteSpace: "nowrap" }}>
             Facebook Clone
           </Typography>
           <Image
             src="/img.png"
             alt="Facebook Image"
-            className={styles.heroImage}
+            className={styles.heroImageLeft}
             width={400}
             height={365}
           />
         </Grid>
-        <Grid size={{ md: 4 }}>
+        <Grid size={{ md: 6 }}>
           <Typography variant={"h3"}>Todo App</Typography>
-
           <Image
             src="/hero-image.png"
             alt="Todo App Screenshot"
-            className={styles.heroImage}
+            className={styles.heroImageRight}
             width={400}
             height={365}
           />
