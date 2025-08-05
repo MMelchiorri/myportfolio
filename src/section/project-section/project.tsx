abfa754f3b44c5afdc85c42e79bc7f26f9186ec9
MMelchiorri <mm.melchiorri@gmail.com>
2025-08-05 08:54:15 +0200

    enhance project layout with improved styling and responsive design

--- src/section/project-section/project.tsx
+++ src/section/project-section/project.tsx
@@ -2,61 +2,43 @@
 
 import React from "react";
 import styles from "./project.module.css";
-import { Box, Paper, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import Image from "next/image";
 import { Grid } from "@mui/material";
 
 const Project: React.ElementType = () => {
   return (
     <div className={styles.container} id={"myProject"}>
-      <div className={styles.projectContainer}>
-        <Grid size={{ md: 6 }}>
-          <Paper className={styles.containerPaper}>
-            <Typography variant={"h3"}>Facebook Clone</Typography>
-            <div style={{ display: "flex", margin: "2rem" }}>
-              <Typography
-                variant={"body2"}
-                className={styles.projectDescription}
-              >
-                Facebook UI Clone – Replica front-end di Facebook creata con
-                HTML, CSS, JS e Axios. Simula interazioni API per esercitarsi
-                nell’integrazione di dati anche senza backend.
-              </Typography>
-              <Image
-                src="/hero-image.png"
-                alt="Hero Image"
-                className={styles.heroImage}
-                width={400}
-                height={365}
-              />
-            </div>
-          </Paper>
+      <Grid
+        container
+        className={styles.projectContainer}
+        justifyContent={"center"}
+        gap={40}
+      >
+        <Grid size={{ md: 4 }}>
+          <Typography variant={"h4"} style={{ whiteSpace: "nowrap" }}>
+            Facebook Clone
+          </Typography>
+          <Image
+            src="/img.png"
+            alt="Facebook Image"
+            className={styles.heroImage}
+            width={400}
+            height={365}
+          />
         </Grid>
-        <Grid size={{ md: 6 }}>
-          <Paper className={styles.containerPaper}>
-            <Typography variant={"h3"}>Todo App</Typography>
-            <div style={{ display: "flex", margin: "2rem" }}>
-              <Typography
-                variant={"body2"}
-                className={styles.projectDescription}
-              >
-                Todo App – Applicazione full-stack per la gestione di attività,
-                sviluppata con Node.js, Express, React, MongoDB e Axios.
-                Permette di creare, aggiornare e cancellare task in tempo reale.
-                Il progetto è completamente containerizzato con Docker per
-                garantire portabilità e facilità di deploy.
-              </Typography>
-              <Image
-                src="/hero-image.png"
-                alt="Todo App Screenshot"
-                className={styles.heroImage}
-                width={400}
-                height={365}
-              />
-            </div>
-          </Paper>
+        <Grid size={{ md: 4 }}>
+          <Typography variant={"h3"}>Todo App</Typography>
+
+          <Image
+            src="/hero-image.png"
+            alt="Todo App Screenshot"
+            className={styles.heroImage}
+            width={400}
+            height={365}
+          />
         </Grid>
-      </div>
+      </Grid>
     </div>
   );
 };
